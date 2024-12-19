28e50c1a81041c3515718b095ae5706b3ea8cc3e
Wagner-github <jjullien.contact@gmail.com>
2024-12-19 11:34:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,13 +84,17 @@ if button2:
 
         # Convertir l'index en format 'YYYY-MM-DD' sans heure et fuseau horaire
         history_data2.index = history_data2.index.date  # Cela garde seulement la date (année-mois-jour
-        if user_date !=0:
+        if user_date:
             try:
-                if user_date in history_data2.index:
+                selected_date = pd.to_datetime(user_date).date()  # Convertir la date saisie en datetime.date
+                if selected_date in history_data2.index:
+                    selected_data = history_data2.loc[selected_date]
                     st.write(f"Données pour {selected_date.strftime('%Y-%m-%d')} :")
-                else:
-                    st.error("La date saisie n'est pas presente dans les données.")
+                    st.write(selected_data)
+                 else:
+                    st.error("La date saisie n'est pas présente dans les données.")
             except ValueError:
                 st.error("Le format de la date est incorrect. Veuillez entrer une date au format YYYY-MM-DD.")
+ 
 
 
